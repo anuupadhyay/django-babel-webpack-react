4f3c84aed90999a12d68443c93ee6dd908d05048
Anu Upadhyay <anu.upadhyay.1994@gmail.com>
2021-02-09 13:05:18 +0530

    first commit

---
+++ blog/static/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require ('path');
+
+module.exports = {
+    entry: {
+        app: './src/index.js'
+    },
+    watch: true,
+    devtool: 'source-map',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                      loader: 'babel-loader',
+                      options: {
+                        presets: ['@babel/react']
+                      }
+                    }
+                ],
+
+            }
+        ]
+    },
+    resolve: {
+        extensions: [
+            '.js'
+        ]
+    }
+}
